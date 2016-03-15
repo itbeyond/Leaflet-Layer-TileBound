c7b1c62d51aa3b200a76cb66749fbceb2a3b73be
David Martin <david@itbeyond.com.au>
2016-03-15 10:05:01 +0800

    Update for 0.7.5 and maxNativeZoom

--- layer/L.TileLayer.Bound.js
+++ layer/L.TileLayer.Bound.js
@@ -1,28 +1,29 @@
 ﻿L.TileLayer.Bound = L.TileLayer.extend({
     getTileUrl: function (tilePoint) {
-        this._adjustTilePoint(tilePoint);
         if (this._shouldtileLoad(tilePoint)) {
-            return L.Util.template(this._url, L.Util.extend({
+            return L.Util.template(this._url, L.extend({
+                r: L.Browser.retina ? '@2x' : '',
                 s: this._getSubdomain(tilePoint),
-                z: this._getZoomForUrl(),
                 x: tilePoint.x,
-                y: tilePoint.y
+                y: this.options.tms ? this._globalTileRange.max.y - tilePoint.y : tilePoint.y,
+                z: this._getZoomForUrl()
             }, this.options));
         } else {
             return this.options.boundsTileUrl;
-        }
+}
     },
 
     _shouldtileLoad: function (tilePoint) {
         if (this.options.showbounds) {
             this.options.showbounds = L.latLngBounds(this.options.showbounds);
             var tileSize = this.options.tileSize,
-            nwPoint = tilePoint.multiplyBy(tileSize),
-            sePoint = nwPoint.add(new L.Point(tileSize, tileSize)),
-            nw = this._map.unproject(nwPoint),
-            se = this._map.unproject(sePoint),
-            bounds = new L.LatLngBounds([nw, se]);
 
+            nwPoint = tilePoint.multiplyBy(tileSize),
+            sePoint = nwPoint.add(new L.Point(tileSize, tileSize));
+            nw = this._map.unproject(nwPoint, tilePoint.z);
+            se = this._map.unproject(sePoint, tilePoint.z);
+            bounds = new L.LatLngBounds([nw, se]);
+ 
             if (!this.options.showbounds.intersects(bounds)) {
                 return false;
             }
